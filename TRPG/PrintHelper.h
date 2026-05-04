d45a23e536413b320fce66ae45f08c4949735b12
DESKTOP-JV9Q3R0\y8268 <y826855@naver.com>
2026-05-04 19:46:22 +0900

    [coding] 출력 헬퍼 추가

---
+++ TRPG/PrintHelper.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <iostream>
+
+using namespace std;
+
+class PrintHelper
+{
+public:
+    static void ShowIntro()
+    {
+        ShowTwoLine();
+        cout << "[ 던전 탈출 텍스트 RPG ]" << '\n';
+        ShowTwoLine();
+    }
+
+    static void ShowTwoLine()
+    {
+        cout << "===========================================" << '\n';
+    }
+
+    static void ShowOneLine()
+    {
+        cout << "------------------------------------" << '\n';
+    }
+};
